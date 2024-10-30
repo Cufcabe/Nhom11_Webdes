b9cb2d413c4e5f44368201787b40d81a49c094bb
Cufcabe <dinhhailong2909@gmail.com>
2024-10-30 20:22:11 +0700

    updated

--- js/layout.js
+++ js/layout.js
@@ -2,17 +2,18 @@
 document.getElementById('mobile-menu-toggle').onclick = function() {
     const mobiledropdown = document.getElementById('mobile-menu');
     mobiledropdown.style.display = mobiledropdown.style.display === 'block' ? 'none' : 'block';
-};
+}
 
 document.getElementById('mobile-menu-can-dropdown').onclick = function() {
     const mobiledropdownMenu = document.getElementById('mobile-dropdown-menu');
     mobiledropdownMenu.style.display = mobiledropdownMenu.style.display === 'block' ? 'none' : 'block';
-};
+}
 
 document.getElementById('mobile-menu-can-dropdown-2').onclick = function() {
     const mobiledropdownMenu = document.getElementById('mobile-dropdown-menu-2');
     mobiledropdownMenu.style.display = mobiledropdownMenu.style.display === 'block' ? 'none' : 'block';
-};
+}
+
 
 //Standard navbar script
 document.addEventListener('DOMContentLoaded', () => {
@@ -35,4 +36,24 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});  
+});
+
+
+//Search bar script
+function closeSearchbox() {
+    document.getElementById("search-box").style.display = "none";
+    document.getElementById("searchbox-background").style.display = "none";
+}
+function openSearchbox() {
+document.getElementById("search-box").style.display = "block";
+document.getElementById("searchbox-background").style.display = "block";
+}
+
+//Contact form
+function openContactForm() {
+    document.getElementById("contact-form-container").style.display = "block";
+}
+
+function closeContactForm() {
+    document.getElementById("contact-form-container").style.display = "none";
+}
